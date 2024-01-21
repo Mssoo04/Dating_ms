12a31e02c931729a1488db2e02da3b002d2dc6ca
friend000 <alstn4980@gmail.com>
2024-01-22 04:36:28 +0900

    pul

--- lib/binding/init_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dating/controller/auth_controller.dart';
-import 'package:get/get.dart';
-
-class InitBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.put(AuthController());
-  }
-}
--- lib/binding/upload_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dating/controller/upload_controller.dart';
-import 'package:get/get.dart';
-
-class UploadBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.put(UploadController());
-  }
-}
